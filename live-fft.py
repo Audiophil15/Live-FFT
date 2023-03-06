25993406fef9367b629565f7170b75142a6cabed
Philippe <audiophil157@gmail.com>
2023-03-06 19:23:31 +0100

    Added two functions to modify the plotted data. Maybe the real thing to focus on is the GUI.

--- live-fft.py
+++ live-fft.py
@@ -12,6 +12,18 @@ def prepareInteractivePlot() :
 
 	return fig, data
 
+def replaceData(data, xdata, ydata) :
+	data.set_xdata(xdata)
+	data.set_ydata(ydata)
+
+def appendData(data, xdata, ydata) :
+	data.set_xdata(data.get_xdata().extend(xdata))
+	data.set_ydata(data.get_ydata().extend(ydata))
+
+def adaptWindow(data) :
+	plt.xlim(min(data.get_xdata())-0.2, max(data.get_xdata())+0.2)
+	plt.ylim(min(data.get_ydata())-0.2, max(data.get_ydata())+0.2)
+
 fig, data = prepareInteractivePlot()
 
 # Number of sample points
@@ -25,17 +37,12 @@ plt.ylim([-0.2,1.2])
 
 for i in range(150) :
 
-	y = 3*np.sin(i*10 * 2.0*pi*x) + 0.5*np.sin(100*i * 2.0*pi*x)
+	y = 3*sin(i*10 * 2.0*pi*x) + 0.5*sin(100*i * 2.0*pi*x)
 	yf = fft(y)
 	xf = fftfreq(N, T)
 
-	xdata = xf[:N//2]
-	ydata = (2.0/N * np.abs(yf))[:N//2]
-
-	plt.ylim([-0.2,max(ydata)+0.2])
-
-	data.set_xdata(xdata)
-	data.set_ydata(ydata)
+	replaceData(data, xf[:N//2], (2.0/N * np.abs(yf))[:N//2])
+	adaptWindow(data)
 
 	fig.canvas.draw()
 	fig.canvas.flush_events()
